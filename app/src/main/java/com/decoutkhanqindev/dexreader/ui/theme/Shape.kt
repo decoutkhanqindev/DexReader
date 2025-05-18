c73a4fba055bff2febba2ba8b52a83645fa18a76
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-19 00:56:20 +0700

    feat: update initial theme to custom Material3 theme

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/ui/theme/Shape.kt
@@ -0,0 +1,21 @@
+package com.decoutkhanqindev.dexreader.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+  small = RoundedCornerShape(
+    topEnd = 4.dp,
+    bottomStart = 4.dp,
+  ),
+  medium = RoundedCornerShape(
+    topEnd = 8.dp,
+    bottomStart = 8.dp,
+  ),
+  large = RoundedCornerShape(
+    topEnd = 16.dp,
+    bottomStart = 16.dp,
+  )
+)
+
