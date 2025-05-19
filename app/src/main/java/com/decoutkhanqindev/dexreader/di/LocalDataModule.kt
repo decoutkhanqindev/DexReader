32cb668e1c18924f1f12792164b1dadbc7ccc6e5
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-19 21:50:39 +0700

    feat: add LocalDataModule for providing ChapterCacheDatabase and ChapterCacheDao

--- app/src/main/java/com/decoutkhanqindev/dexreader/di/LocalDataModule.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/di/LocalDataModule.kt
@@ -1,2 +1,25 @@
 package com.decoutkhanqindev.dexreader.di
 
+import android.content.Context
+import com.decoutkhanqindev.dexreader.data.local.database.ChapterCacheDatabase
+import com.decoutkhanqindev.dexreader.data.local.database.dao.ChapterCacheDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalDataModule {
+  @Provides
+  @Singleton
+  fun provideChapterCacheDB(@ApplicationContext context: Context): ChapterCacheDatabase =
+    ChapterCacheDatabase.getInstance(context)
+
+  @Provides
+  @Singleton
+  fun provideChapterCacheDao(chapterCacheDatabase: ChapterCacheDatabase): ChapterCacheDao =
+    chapterCacheDatabase.chapterCacheDao()
+}
