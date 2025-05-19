f65559b376029f0bc335976bedc8745f1ff0b697
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-20 00:12:40 +0700

    feat: add MangaRepository interface for manga data operations

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/domain/repository/MangaRepository.kt
@@ -0,0 +1,16 @@
+package com.decoutkhanqindev.dexreader.domain.repository
+
+import com.decoutkhanqindev.dexreader.domain.model.Chapter
+import com.decoutkhanqindev.dexreader.domain.model.ChapterPages
+import com.decoutkhanqindev.dexreader.domain.model.Manga
+
+interface MangaRepository {
+  suspend fun getLatestUploadedMangaList(): Result<List<Manga>>
+  suspend fun getTrendingMangaList(): Result<List<Manga>>
+  suspend fun getNewReleaseMangaList(): Result<List<Manga>>
+  suspend fun getCompletedMangaList(): Result<List<Manga>>
+  suspend fun getMangaDetails(mangaId: String): Result<Manga>
+  suspend fun getChapterList(mangaId: String): Result<List<Chapter>>
+  suspend fun getChapterPages(chapterId: String): Result<ChapterPages>
+  suspend fun searchManga(query: String): Result<List<Manga>>
+}
