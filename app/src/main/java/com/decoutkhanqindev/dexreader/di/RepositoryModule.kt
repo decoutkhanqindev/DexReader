68a82a6377e22a53be66b9e66bdf9eaafc7d2e19
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-20 00:12:03 +0700

    feat: add RepositoryModule for providing MangaRepository implementation

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.decoutkhanqindev.dexreader.di
+
+import com.decoutkhanqindev.dexreader.data.repository.MangaRepositoryImpl
+import com.decoutkhanqindev.dexreader.domain.repository.MangaRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+  @Binds
+  fun provideMangaRepository(
+    mangaRepositoryImpl: MangaRepositoryImpl
+  ): MangaRepository
+}
