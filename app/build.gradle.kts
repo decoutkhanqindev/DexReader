7a8066a31de23a5f53ae20f571bda1b8d02be88b
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-19 21:43:20 +0700

    feat: add Jetpack and Third-party Libs below dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
   // Navigation
   implementation(libs.androidx.navigation.compose)
   implementation(libs.kotlinx.serialization.json)
+  implementation(libs.androidx.hilt.navigation.compose)
 
   // Moshi
   implementation(libs.moshi.kotlin)
