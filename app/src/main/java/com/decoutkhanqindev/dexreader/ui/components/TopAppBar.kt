760ec9f068845fbcba53cef1fd7a519dbf8bdbc2
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-21 17:05:58 +0700

    feat: add HomeTopBar and DetailsTopBar composables

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/ui/components/TopAppBar.kt
@@ -0,0 +1,118 @@
+package com.decoutkhanqindev.dexreader.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.decoutkhanqindev.dexreader.R
+import com.decoutkhanqindev.dexreader.ui.theme.DexReaderTheme
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun HomeTopBar(
+  title: String,
+  onMenuClick: () -> Unit,
+  onSearchClick: () -> Unit,
+  modifier: Modifier = Modifier
+) {
+  CenterAlignedTopAppBar(
+    title = {
+      Text(
+        text = title,
+        style = MaterialTheme.typography.titleLarge
+      )
+    },
+    navigationIcon = {
+      IconButton(onClick = onMenuClick) {
+        Icon(
+          imageVector = Icons.Default.Menu,
+          contentDescription = stringResource(R.string.menu_button_content_description)
+        )
+      }
+    },
+    actions = {
+      IconButton(onClick = onSearchClick) {
+        Icon(
+          imageVector = Icons.Default.Search,
+          contentDescription = stringResource(R.string.search_button_content_description)
+        )
+      }
+    },
+    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+      containerColor = MaterialTheme.colorScheme.primaryContainer,
+      titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+      navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+      actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+    ),
+    modifier = modifier
+  )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsTopBar(
+  title: String,
+  onBackClick: () -> Unit,
+  canNavigateBack: Boolean,
+  modifier: Modifier = Modifier
+) {
+  CenterAlignedTopAppBar(
+    title = {
+      Text(
+        text = title,
+        style = MaterialTheme.typography.titleLarge
+      )
+    },
+    navigationIcon = {
+      if (canNavigateBack) {
+        IconButton(onClick = onBackClick) {
+          Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            contentDescription = stringResource(R.string.back_button_content_description)
+          )
+        }
+      }
+    },
+    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+      containerColor = MaterialTheme.colorScheme.primaryContainer,
+      titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+      navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+      actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+    ),
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+  DexReaderTheme {
+    Column() {
+      HomeTopBar(
+        title = stringResource(R.string.app_name),
+        onMenuClick = {},
+        onSearchClick = {},
+        modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
+      )
+
+      DetailsTopBar(
+        title = stringResource(R.string.app_name),
+        onBackClick = {},
+        canNavigateBack = true,
+      )
+    }
+  }
+}
