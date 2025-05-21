6e2acf6e069b9cfb7a528dd3327d3470952351a2
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-21 23:50:46 +0700

    feat: add SearchMangaUseCase for querying manga

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/domain/usecase/manga/SearchMangaUseCase.kt
@@ -0,0 +1,12 @@
+package com.decoutkhanqindev.dexreader.domain.usecase.manga
+
+import com.decoutkhanqindev.dexreader.domain.model.Manga
+import com.decoutkhanqindev.dexreader.domain.repository.MangaRepository
+import jakarta.inject.Inject
+
+class SearchMangaUseCase @Inject constructor(
+  private val mangaRepository: MangaRepository,
+) {
+  suspend operator fun invoke(query: String): Result<List<Manga>> =
+    mangaRepository.searchManga(query = query)
+}
