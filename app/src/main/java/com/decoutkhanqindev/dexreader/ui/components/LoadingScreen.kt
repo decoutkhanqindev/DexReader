d6e72c3d8ecc70d8c98ea857802a44ef75f33039
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-21 23:45:32 +0700

    feat: add LoadingScreen composable

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/ui/components/LoadingScreen.kt
@@ -0,0 +1,29 @@
+package com.decoutkhanqindev.dexreader.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.ui.theme.DexReaderTheme
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+  Box(
+    modifier = modifier,
+    contentAlignment = Alignment.Center
+  ) {
+    CircularProgressIndicator(strokeWidth = 6.dp)
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingScreenPreview() {
+  DexReaderTheme {
+    LoadingScreen(modifier = Modifier.fillMaxSize())
+  }
+}
