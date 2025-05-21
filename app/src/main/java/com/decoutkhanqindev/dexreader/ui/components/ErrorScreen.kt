62676ed19781adb71aacdc0a4e92ef33c7d18a51
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-21 23:45:41 +0700

    feat: add ErrorScreen composable

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/ui/components/ErrorScreen.kt
@@ -0,0 +1,72 @@
+package com.decoutkhanqindev.dexreader.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.R
+import com.decoutkhanqindev.dexreader.ui.theme.DexReaderTheme
+
+@Composable
+fun ErrorScreen(
+  errorMessage: String,
+  onRetryClick: () -> Unit,
+  modifier: Modifier = Modifier,
+) {
+  Column(
+    modifier = modifier,
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Icon(
+      imageVector = Icons.Default.Warning,
+      contentDescription = null,
+      modifier = Modifier.size(100.dp)
+    )
+    Spacer(modifier = Modifier.height(8.dp))
+    Text(
+      text = errorMessage,
+      style = MaterialTheme.typography.titleLarge,
+      textAlign = TextAlign.Center,
+      modifier = Modifier.padding(start = 8.dp)
+    )
+    Spacer(modifier = Modifier.height(8.dp))
+    Button(
+      onClick = onRetryClick,
+    ) {
+      Text(
+        text = stringResource(R.string.retry),
+        style = MaterialTheme.typography.titleMedium,
+        modifier = Modifier.padding(4.dp)
+      )
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorScreenPreview() {
+  DexReaderTheme {
+    ErrorScreen(
+      errorMessage = "Error loading data",
+      onRetryClick = {},
+      modifier = Modifier.fillMaxSize()
+    )
+  }
+}
