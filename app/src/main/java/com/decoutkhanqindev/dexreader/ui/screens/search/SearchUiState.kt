c3a35b8012f309843796d05d081d79f3ce43e9d0
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-28 00:25:17 +0700

    feat: restructure SearchUiState to separate search suggestions and results states for better clarity and functionality

--- app/src/main/java/com/decoutkhanqindev/dexreader/ui/screens/search/SearchUiState.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/ui/screens/search/SearchUiState.kt
@@ -2,9 +2,25 @@ package com.decoutkhanqindev.dexreader.ui.screens.search
 
 import com.decoutkhanqindev.dexreader.domain.model.Manga
 
-sealed interface SearchUiState {
-  data object Idle: SearchUiState
-  data object Loading : SearchUiState
-  data object Error : SearchUiState
-  data class Success(val results: List<Manga> = emptyList()): SearchUiState
+sealed interface SearchSuggestionsUiState {
+  data object Loading : SearchSuggestionsUiState
+  data object Error : SearchSuggestionsUiState
+  data object Success : SearchSuggestionsUiState
+}
+
+sealed interface SearchResultsUiState {
+  data object FirstPageLoading : SearchResultsUiState
+  data object FirstPageError : SearchResultsUiState
+  data class Content(
+    val items: List<Manga> = emptyList(),
+    val currentPage: Int = 0,
+    val nextPageState: SearchMangaListNextPageState = SearchMangaListNextPageState.IDLE
+  ) : SearchResultsUiState
+}
+
+enum class SearchMangaListNextPageState {
+  LOADING,
+  ERROR,
+  IDLE,
+  NO_MORE_ITEMS,
 }
