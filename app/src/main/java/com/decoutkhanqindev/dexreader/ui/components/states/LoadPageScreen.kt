4b3ffbff2de6dd107ea85de394683e6ce744f288
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-27 15:38:52 +0700

    feat: add loading and error screens for page loading

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/ui/components/states/LoadPageScreen.kt
@@ -0,0 +1,77 @@
+package com.decoutkhanqindev.dexreader.ui.components.states
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.R
+
+@Composable
+fun FirstPageLoadingScreen(modifier: Modifier = Modifier) {
+  Box(
+    modifier = modifier,
+    contentAlignment = Alignment.Center
+  ) {
+    CircularProgressIndicator(
+      strokeWidth = 6.dp,
+      modifier = Modifier.padding(bottom = 16.dp)
+    )
+  }
+}
+
+@Composable
+fun NextPageLoadingScreen(modifier: Modifier = Modifier) {
+  Box(
+    modifier = modifier,
+    contentAlignment = Alignment.BottomCenter
+  ) {
+    CircularProgressIndicator(
+      strokeWidth = 6.dp,
+      modifier = Modifier.padding(bottom = 16.dp)
+    )
+  }
+}
+
+
+@Composable
+fun LoadPageErrorScreen(
+  message: String,
+  onRetry: () -> Unit,
+  modifier: Modifier = Modifier,
+) {
+  Row(
+    modifier = modifier,
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.Center
+  ) {
+    Text(
+      text = message,
+      style = MaterialTheme.typography.titleMedium,
+      fontWeight = FontWeight.Light,
+      fontStyle = FontStyle.Italic,
+      textAlign = TextAlign.Center,
+    )
+    IconButton(onClick = onRetry) {
+      Icon(
+        imageVector = Icons.Default.Refresh,
+        contentDescription = stringResource(R.string.retry),
+      )
+    }
+  }
+}
+
