0b249731abd9f1d3977a82a5976f7b4f69133344
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-30 15:05:22 +0700

    feat: implement MainActivity for app entry point and theme setup

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/MainActivity.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/MainActivity.kt
@@ -1,18 +1,20 @@
-package com.decoutkhanqindev.dexreader
+package com.decoutkhanqindev.dexreader.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.decoutkhanqindev.dexreader.ui.theme.DexReaderTheme
+import com.decoutkhanqindev.dexreader.presentation.theme.DexReaderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
       DexReaderTheme {
-
+        DexReaderApp()
       }
     }
   }
