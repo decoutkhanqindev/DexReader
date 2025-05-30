24ba54cbdd062f8b2a0f9443dfb1c2776584b9d3
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-30 15:05:57 +0700

    refactor: reorganize manga details components and update package structure for clarity

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/actions/ReadingButton.kt
@@ -0,0 +1,37 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.manga_details.components.actions
+
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.decoutkhanqindev.dexreader.R
+
+@Composable
+fun ReadingButton(
+  isReading: Boolean,
+  onReadingClick: (String) -> Unit,
+  modifier: Modifier = Modifier,
+) {
+  OutlinedButton(
+    onClick = { onReadingClick },
+    shape = MaterialTheme.shapes.large,
+    colors = ButtonDefaults.outlinedButtonColors(
+      containerColor = MaterialTheme.colorScheme.surface.copy(0.7f)
+    ),
+    modifier = modifier
+  ) {
+    Text(
+      text = if (isReading)
+        stringResource(R.string.continue_reading)
+      else
+        stringResource(R.string.start_reading),
+      style = MaterialTheme.typography.titleMedium,
+      color = MaterialTheme.colorScheme.inverseSurface,
+      fontWeight = FontWeight.ExtraBold,
+    )
+  }
+}
