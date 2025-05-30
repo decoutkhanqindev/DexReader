d3e971da10fe6f448a528fd74680315e7c8e15ab
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-30 15:03:08 +0700

    feat: add NextPageLoadingIndicator component for loading state indication

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/components/common/indicators/NextPageLoadingIndicator.kt
@@ -0,0 +1,18 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.components.common.indicators
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NextPageLoadingIndicator(modifier: Modifier = Modifier) {
+  Box(
+    modifier = modifier,
+    contentAlignment = Alignment.BottomCenter
+  ) {
+    CircularProgressIndicator(strokeWidth = 6.dp)
+  }
+}
