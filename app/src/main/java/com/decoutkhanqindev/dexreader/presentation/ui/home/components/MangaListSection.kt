2d4f8ae4243b63342804edafc1d9c29c929d9e4e
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-05-30 15:00:44 +0700

    refactor: reorganize home components and update package structure for clarity

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/home/components/MangaListSection.kt
@@ -0,0 +1,35 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.home.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.domain.model.Manga
+import com.decoutkhanqindev.dexreader.presentation.ui.components.common.lists.HorizontalMangaList
+
+
+@Composable
+fun MangaListSection(
+  title: String,
+  mangaList: List<Manga>,
+  onSelectedManga: (Manga) -> Unit,
+  modifier: Modifier = Modifier
+) {
+  Column(modifier = modifier) {
+    MangaListTitle(
+      title = title,
+      modifier = Modifier.padding(
+        start = 16.dp,
+        top = 8.dp,
+        bottom = 4.dp
+      )
+    )
+    HorizontalMangaList(
+      mangaList = mangaList,
+      onSelectedManga = onSelectedManga,
+      modifier = Modifier.fillMaxWidth()
+    )
+  }
+}
