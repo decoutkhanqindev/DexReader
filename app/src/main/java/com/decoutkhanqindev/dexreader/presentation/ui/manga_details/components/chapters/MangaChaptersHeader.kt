ff886d54d5102c5bdd23478bf6788ed9cb947f51
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-01 17:33:49 +0700

    feat: update MangaChaptersHeader to use MaterialTheme for title styling

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/chapters/MangaChaptersHeader.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/chapters/MangaChaptersHeader.kt
@@ -2,9 +2,14 @@ package com.decoutkhanqindev.dexreader.presentation.ui.manga_details.components.
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.decoutkhanqindev.dexreader.R
 
 @Composable
 fun MangaChaptersHeader(
@@ -17,7 +22,10 @@ fun MangaChaptersHeader(
     modifier = modifier,
     verticalAlignment = Alignment.CenterVertically,
   ) {
-    MangaChaptersTitle(
+    Text(
+      text = stringResource(R.string.chapters),
+      style = MaterialTheme.typography.titleLarge,
+      fontWeight = FontWeight.ExtraBold,
       modifier = Modifier
         .weight(0.5f)
         .fillMaxWidth()
