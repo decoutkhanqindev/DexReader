58437c3724c0fd3b8fbe33a3fc4ae63f00f86f3e
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-01 17:25:09 +0700

    feat: add ChapterDetailsUiState and ChapterPagesUiState for managing reader state

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/reader/ReaderUiState.kt
@@ -0,0 +1,18 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.reader
+
+import com.decoutkhanqindev.dexreader.domain.model.ChapterPages
+
+data class ChapterDetailsUiState(
+  val volume: String = "",
+  val chapterNumber: String = "",
+  val title: String = ""
+)
+
+sealed interface ChapterPagesUiState {
+  data object Loading : ChapterPagesUiState
+  data object Error : ChapterPagesUiState
+  data class Success(
+    val currentChapterPage: Int = 1,
+    val chapterPages: ChapterPages
+  ) : ChapterPagesUiState
+}
