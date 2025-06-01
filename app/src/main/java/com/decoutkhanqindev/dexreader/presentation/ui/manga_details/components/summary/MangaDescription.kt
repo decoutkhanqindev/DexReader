041b51366dea5ed9e88b927ae136adc8e012971c
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-01 17:32:23 +0700

    feat: enhance MangaDescription and MangaSummarySection with expandable description and improved title styling

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/summary/MangaDescription.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/summary/MangaDescription.kt
@@ -1,6 +1,12 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.manga_details.components.summary
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,9 +14,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import com.decoutkhanqindev.dexreader.R
 
 @Composable
 fun MangaDescription(
@@ -19,12 +28,28 @@ fun MangaDescription(
 ) {
   var isExpanded by rememberSaveable { mutableStateOf(false) }
 
-  Text(
-    text = description,
-    style = MaterialTheme.typography.bodyMedium,
-    fontWeight = FontWeight.Bold,
-    maxLines = if (isExpanded) Int.MAX_VALUE else 3,
-    overflow = TextOverflow.Ellipsis,
-    modifier = modifier.clickable { isExpanded = !isExpanded }
-  )
+  Column(modifier = modifier) {
+    Text(
+      text = description,
+      style = MaterialTheme.typography.bodyMedium,
+      fontWeight = FontWeight.Bold,
+      maxLines = if (isExpanded) Int.MAX_VALUE else 3,
+      overflow = TextOverflow.Ellipsis,
+      modifier = Modifier.fillMaxWidth()
+    )
+    IconButton(
+      onClick = { isExpanded = !isExpanded },
+      modifier = Modifier.align(Alignment.End)
+    ) {
+      Icon(
+        imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+        contentDescription = if (isExpanded)
+          stringResource(R.string.icon_expand_less)
+        else
+          stringResource(
+            R.string.icon_expand_more
+          )
+      )
+    }
+  }
 }
--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/summary/MangaSummarySection.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/summary/MangaSummarySection.kt
@@ -3,10 +3,16 @@ package com.decoutkhanqindev.dexreader.presentation.ui.manga_details.components.
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.R
 import com.decoutkhanqindev.dexreader.domain.model.Manga
+
 
 @Composable
 fun MangaSummarySection(
@@ -15,7 +21,10 @@ fun MangaSummarySection(
   modifier: Modifier = Modifier
 ) {
   Column(modifier = modifier) {
-    MangaSummaryTitle(
+    Text(
+      text = stringResource(R.string.summary),
+      style = MaterialTheme.typography.titleLarge,
+      fontWeight = FontWeight.ExtraBold,
       modifier = Modifier
         .padding(horizontal = 4.dp)
         .padding(bottom = 12.dp)
@@ -23,11 +32,11 @@ fun MangaSummarySection(
     MangaDescription(
       description = manga.description,
       modifier = Modifier
+        .fillMaxWidth()
         .padding(horizontal = 4.dp)
-        .padding(bottom = 8.dp),
     )
     MangaGenreList(
-      manga = manga,
+      genreList = manga.genres,
       onSelectedGenre = onSelectedGenre,
       modifier = Modifier.fillMaxWidth()
     )
