f3bae37a4e44db976fe3d423c82c11ae3971af22
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-01 17:32:00 +0700

    feat: update MangaListSection to use MaterialTheme for title styling

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/home/components/MangaListSection.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/home/components/MangaListSection.kt
@@ -3,8 +3,11 @@ package com.decoutkhanqindev.dexreader.presentation.ui.home.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.decoutkhanqindev.dexreader.domain.model.Manga
 import com.decoutkhanqindev.dexreader.presentation.ui.common.lists.HorizontalMangaList
@@ -18,8 +21,11 @@ fun MangaListSection(
   modifier: Modifier = Modifier
 ) {
   Column(modifier = modifier) {
-    MangaListTitle(
-      title = title,
+    Text(
+      text = title,
+      style = MaterialTheme.typography.titleLarge,
+      fontWeight = FontWeight.ExtraBold,
+      color = MaterialTheme.colorScheme.onPrimaryContainer,
       modifier = Modifier.padding(
         start = 16.dp,
         top = 8.dp,
