b3f8dc997a011c9e9f770e750b9f3b8df5df47e0
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-01 22:03:43 +0700

    feat: add ChapterPageImage component for zoomable image display

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/reader/components/ChapterPageImage.kt
@@ -0,0 +1,33 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.reader.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil3.request.CachePolicy
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import me.saket.telephoto.zoomable.coil3.ZoomableAsyncImage
+import me.saket.telephoto.zoomable.rememberZoomableImageState
+
+@Composable
+fun ChapterPageImage(
+  imageUrl: String,
+  modifier: Modifier = Modifier,
+) {
+  val zoomableState = rememberZoomableImageState()
+
+  ZoomableAsyncImage(
+    model = ImageRequest.Builder(LocalContext.current)
+      .data(imageUrl)
+      .crossfade(true)
+      .memoryCachePolicy(CachePolicy.ENABLED)
+      .diskCachePolicy(CachePolicy.ENABLED)
+      .build(),
+    contentDescription = null,
+    contentScale = ContentScale.Fit,
+    state = zoomableState,
+    modifier = modifier
+  )
+}
+
