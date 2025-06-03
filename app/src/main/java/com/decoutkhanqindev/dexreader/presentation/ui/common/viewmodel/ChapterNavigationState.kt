3b3d0f2a0c5ae30e913619d36e9f7e751ad24581
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-04 00:14:09 +0700

    refactor: remove ChapterNavigationViewModel and related classes

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/viewmodel/ChapterNavigationState.kt
+++
@@ -1,8 +0,0 @@
-package com.decoutkhanqindev.dexreader.presentation.ui.common.viewmodel
-
-data class ChapterNavigationState(
-  val currentChapterId: String,
-  val previousChapterId: String?,
-  val nextChapterId: String?,
-  val canLoadNextChapterPages: Boolean
-)
--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/viewmodel/ChapterRelation.kt
+++
@@ -1,7 +0,0 @@
-package com.decoutkhanqindev.dexreader.presentation.ui.common.viewmodel
-
- data class ChapterRelation(
-  val id: String,
-  val previousId: String?,
-  val nextId: String?
-)
