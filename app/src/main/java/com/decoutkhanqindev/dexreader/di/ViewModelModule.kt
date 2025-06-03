235233a175b8b0cecd7e54ab68c9071092898e28
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-04 00:14:59 +0700

    refactor: remove redundant ChapterNavigationViewModel provision from ViewModelModule

--- app/src/main/java/com/decoutkhanqindev/dexreader/di/ViewModelModule.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/di/ViewModelModule.kt
@@ -1,18 +1,15 @@
 package com.decoutkhanqindev.dexreader.di
 
-import com.decoutkhanqindev.dexreader.presentation.ui.common.viewmodel.ChapterNavigationViewModel
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ViewModelModule {
-  @Provides
-  @Singleton
-  fun provideChapterNavigationViewModel(): ChapterNavigationViewModel {
-    return ChapterNavigationViewModel()
-  }
+//  @Provides
+//  @Singleton
+//  fun provideChapterNavigationViewModel(): ChapterNavigationViewModel {
+//    return ChapterNavigationViewModel()
+//  }
 }
