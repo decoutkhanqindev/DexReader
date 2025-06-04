2a380e6d6dc705151d481fc5bbfe8aa8540d2ed2
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-04 15:56:43 +0700

    feat: add binding for CacheRepository in RepositoryModule

--- app/src/main/java/com/decoutkhanqindev/dexreader/di/RepositoryModule.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.decoutkhanqindev.dexreader.di
 
+import com.decoutkhanqindev.dexreader.data.repository.CacheRepositoryImpl
 import com.decoutkhanqindev.dexreader.data.repository.MangaRepositoryImpl
+import com.decoutkhanqindev.dexreader.domain.repository.CacheRepository
 import com.decoutkhanqindev.dexreader.domain.repository.MangaRepository
 import dagger.Binds
 import dagger.Module
@@ -14,4 +16,9 @@ interface RepositoryModule {
   fun provideMangaRepository(
     mangaRepositoryImpl: MangaRepositoryImpl
   ): MangaRepository
+
+  @Binds
+  fun provideCacheRepository (
+    cacheRepositoryImpl: CacheRepositoryImpl
+  ): CacheRepository
 }
