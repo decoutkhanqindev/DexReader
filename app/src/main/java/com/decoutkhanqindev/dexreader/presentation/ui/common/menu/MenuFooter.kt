581dcdb77628f8880657d100079a8f330d8be040
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-04 23:07:06 +0700

    feat: add MenuFooter composable for footer display in navigation menu

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/menu/MenuFooter.kt
@@ -0,0 +1,33 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.common.menu
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.decoutkhanqindev.dexreader.R
+
+@Composable
+fun MenuFooter(modifier: Modifier = Modifier) {
+  Row(
+    modifier = modifier,
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.Center
+  ) {
+    Text(
+      text = stringResource(R.string.decoutkhanqindev),
+      style = MaterialTheme.typography.bodySmall,
+      fontWeight = FontWeight.Light,
+      fontStyle = FontStyle.Italic,
+      textAlign = TextAlign.Center,
+      modifier = Modifier.fillMaxWidth()
+    )
+  }
+}
