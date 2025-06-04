fb4f236a0ac515caa50f0c41fca311e42c5ef9b2
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-04 15:55:12 +0700

    feat: add RoomDBTypeConverters for List<String> serialization in Room database

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/data/utils/RoomDBTypeConverters.kt
@@ -0,0 +1,16 @@
+package com.decoutkhanqindev.dexreader.data.utils
+
+import androidx.room.TypeConverter
+import kotlinx.serialization.json.Json
+
+class RoomDBTypeConverters {
+  @TypeConverter
+  fun fromStringList(list: List<String>?): String? {
+    return list?.let { Json.encodeToString(it) }
+  }
+
+  @TypeConverter
+  fun toStringList(jsonString: String?): List<String>? {
+    return jsonString?.let { Json.decodeFromString<List<String>>(it) }
+  }
+}
