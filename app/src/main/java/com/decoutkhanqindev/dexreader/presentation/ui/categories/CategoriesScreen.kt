9281dac5e15233b40997ca8e5cf08d1dd2048ad4
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-06 00:22:33 +0700

    feat: add CategoriesScreen composable for displaying category content and handling navigation

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/categories/CategoriesScreen.kt
@@ -0,0 +1,46 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.categories
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.decoutkhanqindev.dexreader.R
+import com.decoutkhanqindev.dexreader.presentation.navigation.NavigationDestination
+import com.decoutkhanqindev.dexreader.presentation.ui.categories.components.CategoriesContent
+import com.decoutkhanqindev.dexreader.presentation.ui.common.base.BaseScreen
+
+@Composable
+fun CategoriesScreen(
+  onMenuItemClick: (String) -> Unit,
+  onSearchClick: () -> Unit,
+  onSelectedCategory: (String) -> Unit,
+  viewModel: CategoryViewModel = hiltViewModel(),
+  modifier: Modifier = Modifier
+) {
+  val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+  var route by rememberSaveable {
+    mutableStateOf(NavigationDestination.CategoriesScreen.route)
+  }
+
+  BaseScreen(
+    title = stringResource(R.string.category_menu_item),
+    route = route,
+    onMenuItemClick = onMenuItemClick,
+    onSearchClick = onSearchClick,
+    content = {
+      CategoriesContent(
+        uiState = uiState,
+        onSelectedCategory = onSelectedCategory,
+        onRetry = { viewModel.retry() },
+        modifier = Modifier.fillMaxSize()
+      )
+    },
+    modifier = modifier
+  )
+}
