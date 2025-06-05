b76a62611b2297172a5bc694e3155ad2dcff61c0
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-05 15:57:06 +0700

    refactor: rename CategoryMapper to TagMapper and update related domain model

--- app/src/main/java/com/decoutkhanqindev/dexreader/data/mapper/TagMapper.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/data/mapper/TagMapper.kt
@@ -1,10 +1,10 @@
 package com.decoutkhanqindev.dexreader.data.mapper
 
 import com.decoutkhanqindev.dexreader.data.network.mangadex_api.dto.TagDto
-import com.decoutkhanqindev.dexreader.domain.model.Category
+import com.decoutkhanqindev.dexreader.domain.model.Tag
 
-fun TagDto.toDomain(): Category =
-  Category(
+fun TagDto.toDomain(): Tag =
+  Tag(
     id = id,
     name = attributes.name?.get("en") ?: "Unknown",
     description = attributes.description?.get("en") ?: "No descriptions ..."
