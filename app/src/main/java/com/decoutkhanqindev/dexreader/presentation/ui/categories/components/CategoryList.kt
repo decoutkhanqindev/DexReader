93fc52620314989796ddd16f0140047e62e66a9c
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-06 00:21:53 +0700

    feat: add CategoryList composable for displaying a list of categories

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/categories/components/CategoryList.kt
@@ -0,0 +1,30 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.categories.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.domain.model.Category
+
+@Composable
+fun CategoryList(
+  categoryList: List<Category>,
+  onSelectedCategory: (String) -> Unit,
+  modifier: Modifier = Modifier
+) {
+  FlowRow(
+    modifier = modifier,
+    horizontalArrangement = Arrangement.spacedBy(8.dp),
+    verticalArrangement = Arrangement.spacedBy(8.dp),
+  ) {
+    categoryList.forEach { category ->
+      CategoryItem(
+        category = category,
+        onSelectedCategory = onSelectedCategory,
+        modifier = Modifier.wrapContentWidth()
+      )
+    }
+  }
+}
