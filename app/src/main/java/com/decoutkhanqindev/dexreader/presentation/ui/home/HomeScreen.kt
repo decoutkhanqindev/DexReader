bc81a473dc2350729c82c5a62e73a557afd98e0d
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-05 14:18:41 +0700

    feat: refactor HomeScreen to use BaseScreen for improved layout and navigation

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/home/HomeScreen.kt
@@ -1,47 +1,46 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.home
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.decoutkhanqindev.dexreader.R
+import com.decoutkhanqindev.dexreader.presentation.navigation.NavigationDestination
+import com.decoutkhanqindev.dexreader.presentation.ui.common.base.BaseScreen
 import com.decoutkhanqindev.dexreader.presentation.ui.home.components.HomeContent
-import com.decoutkhanqindev.dexreader.presentation.ui.home.components.HomeTopBar
 
 @Composable
 fun HomeScreen(
-  onMenuClick: () -> Unit,
+  onMenuItemClick: (String) -> Unit,
   onSearchClick: () -> Unit,
   onSelectedManga: (String) -> Unit,
   viewModel: HomeViewModel = hiltViewModel(),
   modifier: Modifier = Modifier,
 ) {
   val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+  var route by rememberSaveable {
+    mutableStateOf(NavigationDestination.HomeScreen.route)
+  }
 
-  Scaffold(
-    modifier = modifier,
-    topBar = {
-      HomeTopBar(
-        onMenuClick = onMenuClick,
-        onSearchClick = onSearchClick,
-        modifier = Modifier.fillMaxWidth()
-      )
-    },
-    content = { innerPadding ->
+  BaseScreen(
+    title = stringResource(R.string.app_name),
+    route = route,
+    onMenuItemClick = onMenuItemClick,
+    onSearchClick = onSearchClick,
+    content = {
       HomeContent(
         uiState = uiState,
         onSelectedManga = onSelectedManga,
         onRetry = { viewModel.retry() },
-        modifier = Modifier
-          .padding(innerPadding)
-          .fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
       )
     },
+    modifier = modifier
   )
 }
-
-
