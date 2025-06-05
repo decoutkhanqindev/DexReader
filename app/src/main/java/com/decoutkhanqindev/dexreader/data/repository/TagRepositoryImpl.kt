1eb64e3564e4bca9f5268f4b649cb393630bba82
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-05 15:57:15 +0700

    refactor: rename CategoryRepository and CategoryRepositoryImpl to TagRepository and TagRepositoryImpl

--- app/src/main/java/com/decoutkhanqindev/dexreader/data/repository/TagRepositoryImpl.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/data/repository/TagRepositoryImpl.kt
@@ -3,18 +3,18 @@ package com.decoutkhanqindev.dexreader.data.repository
 import com.decoutkhanqindev.dexreader.data.mapper.toDomain
 import com.decoutkhanqindev.dexreader.data.network.mangadex_api.MangaDexApiService
 import com.decoutkhanqindev.dexreader.di.UploadUrlQualifier
-import com.decoutkhanqindev.dexreader.domain.model.Category
 import com.decoutkhanqindev.dexreader.domain.model.Manga
-import com.decoutkhanqindev.dexreader.domain.repository.CategoryRepository
+import com.decoutkhanqindev.dexreader.domain.model.Tag
+import com.decoutkhanqindev.dexreader.domain.repository.TagRepository
 import com.decoutkhanqindev.dexreader.utils.runSuspendCatching
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
-class CategoryRepositoryImpl @Inject constructor(
+class TagRepositoryImpl @Inject constructor(
   private val mangaDexApiService: MangaDexApiService,
   @UploadUrlQualifier private val uploadUrl: String
-) : CategoryRepository {
-  override suspend fun getTagList(): Result<List<Category>> =
+) : TagRepository {
+  override suspend fun getTagList(): Result<List<Tag>> =
     runSuspendCatching(Dispatchers.IO) {
       mangaDexApiService.getTagList().data.map { it.toDomain() }
     }
