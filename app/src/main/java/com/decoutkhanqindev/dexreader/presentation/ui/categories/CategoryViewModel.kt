41d2427c8dc92ff3bcfc47cca259f02ef2e51474
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-06 00:21:35 +0700

    refactor: implement CategoryViewModel to manage category state and fetch tag list

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/categories/CategoryViewModel.kt
@@ -0,0 +1,61 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.categories
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.decoutkhanqindev.dexreader.domain.usecase.category.GetTagListUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryViewModel @Inject constructor(
+  private val getTagListUseCase: GetTagListUseCase,
+) : ViewModel() {
+  private val _uiState = MutableStateFlow<CategoriesUiState>(CategoriesUiState.Loading)
+  val uiState: StateFlow<CategoriesUiState> = _uiState.asStateFlow()
+
+  init {
+    fetchTagList()
+  }
+
+  private fun fetchTagList() {
+    viewModelScope.launch {
+      _uiState.value = CategoriesUiState.Loading
+
+      val tagListResult = getTagListUseCase()
+      tagListResult
+        .onSuccess {
+          val genreList = it.filter { it.group == GENRE }
+          val themeList = it.filter { it.group == THEME }
+          val formatList = it.filter { it.group == FORMAT }
+          val contentList = it.filter { it.group == CONTENT }
+          _uiState.value = CategoriesUiState.Success(
+            genreList = genreList,
+            themeList = themeList,
+            formatList = formatList,
+            contentList = contentList
+          )
+        }
+        .onFailure {
+          _uiState.value = CategoriesUiState.Error
+          Log.e(TAG, "fetchTagList have error: ${it.stackTraceToString()}")
+        }
+    }
+  }
+
+  fun retry() {
+    fetchTagList()
+  }
+
+  companion object {
+    private const val TAG = "CategoryViewModel"
+    private const val GENRE = "genre"
+    private const val THEME = "theme"
+    private const val FORMAT = "format"
+    private const val CONTENT = "content"
+  }
+}
