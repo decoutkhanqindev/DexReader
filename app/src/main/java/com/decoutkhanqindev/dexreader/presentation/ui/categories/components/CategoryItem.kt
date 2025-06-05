ff8df142fe9fd115916641f0f3efb313ae13d9ea
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-06 00:21:46 +0700

    feat: add CategoryItem composable for displaying category details

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/categories/components/CategoryItem.kt
@@ -0,0 +1,36 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.categories.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.domain.model.Category
+
+@Composable
+fun CategoryItem(
+  category: Category,
+  onSelectedCategory: (String) -> Unit,
+  modifier: Modifier = Modifier
+) {
+  Card(
+    onClick = { onSelectedCategory(category.id) },
+    shape = MaterialTheme.shapes.large,
+    modifier = modifier,
+  ) {
+    Text(
+      text = category.name,
+      style = MaterialTheme.typography.bodyMedium,
+      fontWeight = FontWeight.Bold,
+      textAlign = TextAlign.Center,
+      modifier = Modifier
+        .padding(8.dp)
+        .wrapContentWidth()
+    )
+  }
+}
