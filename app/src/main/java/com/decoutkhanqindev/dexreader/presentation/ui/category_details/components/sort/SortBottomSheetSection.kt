433b471a1f32e59f806feb66154dade32ad3c6d1
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-07 01:28:43 +0700

    feat: add SortBottomSheetSection composable for sorting options in category details

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/category_details/components/sort/SortBottomSheetSection.kt
@@ -0,0 +1,90 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.category_details.components.sort
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.R
+import com.decoutkhanqindev.dexreader.presentation.ui.category_details.CategoryDetailsCriteriaUiState
+import com.decoutkhanqindev.dexreader.presentation.ui.category_details.SortCriteria
+import com.decoutkhanqindev.dexreader.presentation.ui.category_details.components.actions.ApplyButton
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SortBottomSheetSection(
+  onDismiss: () -> Unit,
+  criteriaState: CategoryDetailsCriteriaUiState,
+  onApplyClick: (
+    criteriaId: String,
+    orderId: String
+  ) -> Unit,
+  modifier: Modifier = Modifier
+) {
+  val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+  var selectedCriteriaId by remember {
+    mutableStateOf<String>(SortCriteria.LatestUpdate.id)
+  }
+  var selectedOrderId by remember {
+    mutableStateOf(criteriaState.lastUpdatedOrderId)
+  }
+
+  ModalBottomSheet(
+    onDismissRequest = onDismiss,
+    sheetState = sheetState,
+    modifier = modifier
+  ) {
+    Text(
+      text = stringResource(R.string.sort_options),
+      style = MaterialTheme.typography.titleLarge,
+      fontWeight = FontWeight.ExtraBold,
+      textAlign = TextAlign.Center,
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 4.dp)
+        .padding(bottom = 16.dp)
+    )
+
+    VerticalGridSortCriteriaList(
+      selectedCriteriaId = selectedCriteriaId,
+      onSelectedItem = { selectedCriteriaId = it },
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 4.dp)
+        .padding(bottom = 12.dp)
+    )
+
+    SortOrderOptions(
+      selectedOrderId = selectedOrderId!!,
+      onSelectedOption = { selectedOrderId = it },
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 4.dp)
+        .padding(bottom = 24.dp)
+    )
+
+    ApplyButton(
+      onApply = {
+        onApplyClick(selectedCriteriaId, selectedOrderId!!)
+        onDismiss()
+      },
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 4.dp)
+        .padding(bottom = 12.dp)
+    )
+  }
+}
+
