137a6efd987b824518da25b092606547f9ef6b7d
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-06 16:42:23 +0700

    refactor: update route reference in HomeScreen to use HomeDestination

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/home/HomeScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,8 +23,8 @@ fun HomeScreen(
   modifier: Modifier = Modifier,
 ) {
   val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-  var route by rememberSaveable {
-    mutableStateOf(NavigationDestination.HomeScreen.route)
+  val route by rememberSaveable {
+    mutableStateOf(NavigationDestination.HomeDestination.route)
   }
 
   BaseScreen(
