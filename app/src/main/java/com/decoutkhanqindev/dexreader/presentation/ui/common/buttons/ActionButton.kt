7baf6d2d451f99abecf49ab87f849e8a305579f2
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-07 01:14:07 +0700

    feat: add ActionButton composable for reusable button functionality

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/buttons/ActionButton.kt
@@ -0,0 +1,31 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.common.buttons
+
+import androidx.compose.foundation.border
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ActionButton(
+  enabled: Boolean = true,
+  onClick: () -> Unit,
+  content: @Composable () -> Unit,
+  modifier: Modifier = Modifier
+) {
+  Button(
+    onClick = onClick,
+    enabled = enabled,
+    shape = MaterialTheme.shapes.medium,
+    colors = ButtonDefaults.outlinedButtonColors(
+      containerColor = MaterialTheme.colorScheme.surface.copy(0.5f)
+    ),
+    modifier = modifier.border(
+      width = 1.dp,
+      color = MaterialTheme.colorScheme.onPrimaryContainer,
+      shape = MaterialTheme.shapes.medium
+    )
+  ) { content() }
+}
