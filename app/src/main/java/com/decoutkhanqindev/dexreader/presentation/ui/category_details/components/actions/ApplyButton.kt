b1de53f43bab691b42e1a9e1a1e42cf287ec9e11
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-07 01:14:28 +0700

    feat: add ApplyButton composable for applying actions

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/category_details/components/actions/ApplyButton.kt
@@ -0,0 +1,32 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.category_details.components.actions
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.R
+
+@Composable
+fun ApplyButton(
+  onApply: () -> Unit,
+  modifier: Modifier = Modifier
+) {
+  Button(
+    shape = MaterialTheme.shapes.medium,
+    onClick = onApply,
+    modifier = modifier.height(48.dp)
+  ) {
+    Text(
+      text = stringResource(R.string.apply),
+      style = MaterialTheme.typography.titleMedium,
+      fontWeight = FontWeight.ExtraBold,
+      textAlign = TextAlign.Center
+    )
+  }
+}
