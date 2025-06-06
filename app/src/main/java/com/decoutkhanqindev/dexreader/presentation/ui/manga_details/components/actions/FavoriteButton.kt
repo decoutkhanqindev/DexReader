e35feb26bc79395d54f4b6479fd930644321cdc7
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-07 01:15:09 +0700

    feat: replace Button with ActionButton in FavoriteButton and ReadingButton for consistency

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/actions/FavoriteButton.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/actions/FavoriteButton.kt
@@ -1,16 +1,13 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.manga_details.components.actions
 
-import androidx.compose.foundation.border
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import com.decoutkhanqindev.dexreader.R
+import com.decoutkhanqindev.dexreader.presentation.ui.common.buttons.ActionButton
 
 @Composable
 fun FavoriteButton(
@@ -18,23 +15,16 @@ fun FavoriteButton(
   onFavoriteClick: (String) -> Unit,
   modifier: Modifier = Modifier,
 ) {
-  Button(
+  ActionButton(
     onClick = { onFavoriteClick },
-    shape = MaterialTheme.shapes.medium,
-    colors = ButtonDefaults.outlinedButtonColors(
-      containerColor = MaterialTheme.colorScheme.surface.copy(0.5f)
-    ),
-    modifier = modifier.border(
-      width = 1.dp,
-      color = MaterialTheme.colorScheme.onPrimaryContainer,
-      shape = MaterialTheme.shapes.medium
-    )
-  ) {
-    Text(
-      text = if (isFavorite) stringResource(R.string.unfavorite) else stringResource(R.string.favorite),
-      color = MaterialTheme.colorScheme.inverseSurface,
-      style = MaterialTheme.typography.titleMedium,
-      fontWeight = FontWeight.ExtraBold,
-    )
-  }
+    content = {
+      Text(
+        text = if (isFavorite) stringResource(R.string.unfavorite) else stringResource(R.string.favorite),
+        color = MaterialTheme.colorScheme.inverseSurface,
+        style = MaterialTheme.typography.titleMedium,
+        fontWeight = FontWeight.ExtraBold,
+      )
+    },
+    modifier = modifier
+  )
 }
--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/actions/ReadingButton.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/manga_details/components/actions/ReadingButton.kt
@@ -1,16 +1,13 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.manga_details.components.actions
 
-import androidx.compose.foundation.border
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import com.decoutkhanqindev.dexreader.R
+import com.decoutkhanqindev.dexreader.presentation.ui.common.buttons.ActionButton
 
 @Composable
 fun ReadingButton(
@@ -19,27 +16,20 @@ fun ReadingButton(
   onReadingClick: () -> Unit,
   modifier: Modifier = Modifier,
 ) {
-  Button(
-    onClick = onReadingClick,
+  ActionButton(
     enabled = canRead,
-    shape = MaterialTheme.shapes.medium,
-    colors = ButtonDefaults.outlinedButtonColors(
-      containerColor = MaterialTheme.colorScheme.surface.copy(0.5f)
-    ),
-    modifier = modifier.border(
-      width = 1.dp,
-      color = MaterialTheme.colorScheme.onPrimaryContainer,
-      shape = MaterialTheme.shapes.medium
-    )
-  ) {
-    Text(
-      text = if (isReading)
-        stringResource(R.string.continue_reading)
-      else
-        stringResource(R.string.start_reading),
-      style = MaterialTheme.typography.titleMedium,
-      color = MaterialTheme.colorScheme.inverseSurface,
-      fontWeight = FontWeight.ExtraBold,
-    )
-  }
+    onClick = onReadingClick,
+    content = {
+      Text(
+        text = if (isReading)
+          stringResource(R.string.continue_reading)
+        else
+          stringResource(R.string.start_reading),
+        style = MaterialTheme.typography.titleMedium,
+        color = MaterialTheme.colorScheme.inverseSurface,
+        fontWeight = FontWeight.ExtraBold,
+      )
+    },
+    modifier = modifier
+  )
 }
