cae14608e7877eafe57c5184ec3c80a206b03568
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-07 16:08:02 +0700

    feat: enhance CategoryDetailsScreen with sorting and filtering functionality

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/category_details/CategoryDetailsScreen.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/category_details/CategoryDetailsScreen.kt
@@ -1,11 +1,18 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.category_details
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.decoutkhanqindev.dexreader.presentation.ui.category_details.components.CategoryDetailsBottomBar
 import com.decoutkhanqindev.dexreader.presentation.ui.category_details.components.CategoryDetailsContent
 import com.decoutkhanqindev.dexreader.presentation.ui.common.base.BaseDetailsScreen
 
@@ -20,27 +27,42 @@ fun CategoryDetailsScreen(
   val categoryDetailsUiState by viewModel.categoryDetailsUiState.collectAsStateWithLifecycle()
   val categoryCriteriaUiState by viewModel.categoryCriteriaUiState.collectAsStateWithLifecycle()
   val categoryTitle = viewModel.categoryTitleFromArg
+  var isSortSheetVisible by rememberSaveable { mutableStateOf(false) }
+  var isFilterSheetVisible by rememberSaveable { mutableStateOf(false) }
 
   BaseDetailsScreen(
     title = categoryTitle,
     onNavigateBack = onNavigateBack,
     isSearchEnabled = true,
     onSearchClick = onSearchClick,
+    bottomBar = {
+      CategoryDetailsBottomBar(
+        onSortClick = { isSortSheetVisible = true },
+        onFilterClick = { isFilterSheetVisible = true },
+        modifier = Modifier.fillMaxWidth().height(80.dp)
+      )
+    },
     content = {
       CategoryDetailsContent(
         categoryDetailsUiState = categoryDetailsUiState,
         categoryCriteriaUiState = categoryCriteriaUiState,
+        isSortSheetVisible = isSortSheetVisible,
+        onSortSheetDismiss = { isSortSheetVisible = false },
         onSortApplyClick = { criteriaId, orderId ->
           viewModel.updateSortingCriteria(
             criteriaId = criteriaId,
             orderId = orderId
           )
+          isSortSheetVisible = false
         },
+        isFilterSheetVisible = isFilterSheetVisible,
+        onFilterSheetDismiss = { isFilterSheetVisible = false },
         onFilterApplyClick = { statusValueIds, contentRatingValueIds ->
           viewModel.updateFilteringCriteria(
             statusValueIds = statusValueIds,
             contentRatingValueIds = contentRatingValueIds
           )
+          isFilterSheetVisible = false
         },
         onSelectedManga = onSelectedManga,
         onFetchMangaListNextPage = { viewModel.fetchMangaListByCategoryNextPage() },
--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/category_details/components/CategoryDetailsContent.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/category_details/components/CategoryDetailsContent.kt
@@ -1,25 +1,37 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.category_details.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.decoutkhanqindev.dexreader.R
 import com.decoutkhanqindev.dexreader.presentation.ui.category_details.CategoryDetailsCriteriaUiState
+import com.decoutkhanqindev.dexreader.presentation.ui.category_details.CategoryDetailsNextPageState
 import com.decoutkhanqindev.dexreader.presentation.ui.category_details.CategoryDetailsUiState
 import com.decoutkhanqindev.dexreader.presentation.ui.category_details.components.filter.FilterBottomSheet
 import com.decoutkhanqindev.dexreader.presentation.ui.category_details.components.sort.SortBottomSheet
+import com.decoutkhanqindev.dexreader.presentation.ui.common.indicators.NextPageLoadingIndicator
+import com.decoutkhanqindev.dexreader.presentation.ui.common.lists.VerticalGridMangaList
+import com.decoutkhanqindev.dexreader.presentation.ui.common.states.ErrorScreen
+import com.decoutkhanqindev.dexreader.presentation.ui.common.states.LoadingScreen
+import com.decoutkhanqindev.dexreader.presentation.ui.common.texts.AllItemLoadedMessage
+import com.decoutkhanqindev.dexreader.presentation.ui.common.texts.LoadMoreMessage
+import com.decoutkhanqindev.dexreader.presentation.ui.common.texts.LoadPageErrorMessage
 
 @Composable
 fun CategoryDetailsContent(
   categoryDetailsUiState: CategoryDetailsUiState,
   categoryCriteriaUiState: CategoryDetailsCriteriaUiState,
+  isSortSheetVisible: Boolean,
+  onSortSheetDismiss: () -> Unit,
   onSortApplyClick: (
     criteriaId: String,
     orderId: String
   ) -> Unit,
+  isFilterSheetVisible: Boolean,
+  onFilterSheetDismiss: () -> Unit,
   onFilterApplyClick: (
     statusValueIds: List<String>,
     contentRatingValueIds: List<String>
@@ -30,16 +42,14 @@ fun CategoryDetailsContent(
   onRetry: () -> Unit,
   modifier: Modifier = Modifier
 ) {
-  var isSortSheetVisible by rememberSaveable { mutableStateOf(false) }
-  var isFilterSheetVisible by rememberSaveable { mutableStateOf(true) }
+
 
   if (isSortSheetVisible) {
     SortBottomSheet(
-      onDismiss = { isSortSheetVisible = false },
+      onDismiss = onSortSheetDismiss,
       criteriaState = categoryCriteriaUiState,
       onApplyClick = { criteriaId, orderId ->
         onSortApplyClick(criteriaId, orderId)
-        isSortSheetVisible = false
       },
       modifier = Modifier.fillMaxWidth()
     )
@@ -47,13 +57,66 @@ fun CategoryDetailsContent(
 
   if (isFilterSheetVisible) {
     FilterBottomSheet(
-      onDismiss = { isFilterSheetVisible = false },
+      onDismiss = onFilterSheetDismiss,
       criteriaState = categoryCriteriaUiState,
       onApplyClick = { statusValueIds, contentRatingValueIds ->
         onFilterApplyClick(statusValueIds, contentRatingValueIds)
-        isFilterSheetVisible = false
       },
       modifier = Modifier.fillMaxWidth()
     )
+  }
+
+  when (categoryDetailsUiState) {
+    CategoryDetailsUiState.FirstPageLoading -> LoadingScreen(modifier = modifier)
+
+    CategoryDetailsUiState.FirstPageError -> ErrorScreen(
+      message = stringResource(R.string.oops_something_went_wrong_please_try_again),
+      onRetry = onRetry,
+      modifier = modifier
+    )
+
+    is CategoryDetailsUiState.Content -> {
+      val mangaList = categoryDetailsUiState.mangaList
+      val nextPageState = categoryDetailsUiState.nextPageState
+
+      VerticalGridMangaList(
+        mangaList = mangaList,
+        onSelectedManga = { onSelectedManga(it.id) },
+        loadMoreContent = {
+          when (nextPageState) {
+            CategoryDetailsNextPageState.LOADING -> NextPageLoadingIndicator(
+              modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+            )
+
+            CategoryDetailsNextPageState.ERROR -> LoadPageErrorMessage(
+              message = stringResource(R.string.can_t_load_next_manga_page_please_try_again),
+              onRetry = onRetryFetchMangaListNextPage,
+              modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+            )
+
+            CategoryDetailsNextPageState.IDLE -> LoadMoreMessage(
+              onLoadMore = onFetchMangaListNextPage,
+              modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .padding(bottom = 12.dp)
+            )
+
+            CategoryDetailsNextPageState.NO_MORE_ITEMS -> AllItemLoadedMessage(
+              title = stringResource(R.string.all_mangas_loaded),
+              modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .padding(bottom = 12.dp)
+            )
+          }
+        },
+        modifier = modifier
+      )
+    }
   }
 }
