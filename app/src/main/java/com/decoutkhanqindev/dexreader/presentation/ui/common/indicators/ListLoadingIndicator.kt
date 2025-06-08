63d9df53d6480f7d000c3cfe46ef14444d35778a
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-08 16:45:02 +0700

    refactor: replace CircularProgressIndicator with LinearProgressIndicator in loading indicators

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/indicators/ListLoadingIndicator.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/indicators/ListLoadingIndicator.kt
@@ -1,7 +1,9 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.common.indicators
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,8 +16,12 @@ fun ListLoadingIndicator(modifier: Modifier = Modifier) {
     modifier = modifier,
     contentAlignment = Alignment.Center
   ) {
-    CircularProgressIndicator(
-      strokeWidth = 6.dp, color = MaterialTheme.colorScheme.onPrimaryContainer,
+    LinearProgressIndicator(
+      modifier = Modifier
+        .width(100.dp)
+        .padding(top = 8.dp),
+      color = MaterialTheme.colorScheme.onPrimaryContainer,
+      trackColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
     )
   }
 }
--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/indicators/NextPageLoadingIndicator.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/indicators/NextPageLoadingIndicator.kt
@@ -1,7 +1,9 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.common.indicators
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,8 +16,12 @@ fun NextPageLoadingIndicator(modifier: Modifier = Modifier) {
     modifier = modifier,
     contentAlignment = Alignment.BottomCenter
   ) {
-    CircularProgressIndicator(
-      strokeWidth = 6.dp, color = MaterialTheme.colorScheme.onPrimaryContainer,
+    LinearProgressIndicator(
+      modifier = Modifier
+        .width(100.dp)
+        .padding(top = 8.dp),
+      color = MaterialTheme.colorScheme.onPrimaryContainer,
+      trackColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
     )
   }
 }
