2a4e18e58f1fe5c16b213b5b63fb5fc3f41aa537
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-08 16:05:35 +0700

    feat: add UserProfileDto for user profile representation in Firestore

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/data/network/firebase/dto/UserProfileDto.kt
@@ -0,0 +1,28 @@
+package com.decoutkhanqindev.dexreader.data.network.firebase.dto
+
+import com.google.firebase.firestore.PropertyName
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.Date
+
+data class UserProfileDto(
+  @get:PropertyName("uid")
+  @set:PropertyName("uid")
+  var id: String = "",
+
+  @get:PropertyName("name")
+  @set:PropertyName("name")
+  var name: String = "",
+
+  @get:PropertyName("email")
+  @set:PropertyName("email")
+  var email: String = "",
+
+  @get:PropertyName("profile_picture_url")
+  @set:PropertyName("profile_picture_url")
+  var profilePictureUrl: String? = null,
+
+  @ServerTimestamp
+  val createdAt: Date? = null,
+  @ServerTimestamp
+  val updatedAt: Date? = null
+)
