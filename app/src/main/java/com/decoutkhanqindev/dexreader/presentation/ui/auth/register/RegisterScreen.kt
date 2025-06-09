00e79cac080b861bad3bac11c006f81f083d66e5
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-09 16:14:57 +0700

    feat: add RegisterScreen composable for user registration

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/auth/register/RegisterScreen.kt
@@ -0,0 +1,34 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.auth.register
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.decoutkhanqindev.dexreader.presentation.ui.auth.register.components.RegisterContent
+
+@Composable
+fun RegisterScreen(
+  onNavigateBack: () -> Unit,
+  onRegisterSuccess: (
+    email: String,
+    password: String,
+  ) -> Unit,
+  viewModel: RegisterViewModel = hiltViewModel(),
+  modifier: Modifier = Modifier
+) {
+  val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+  RegisterContent(
+    uiState = uiState,
+    onEmailChange = { viewModel.updateEmailField(email = it) },
+    onPasswordChange = { viewModel.updatePasswordField(password = it) },
+    onConfirmPasswordChange = { viewModel.updateConfirmPasswordField(confirmPassword = it) },
+    onNameChange = { viewModel.updateNameField(name = it) },
+    onSubmitClick = { viewModel.registerUser() },
+    onRegisterSuccess = onRegisterSuccess,
+    onNavigateBack = onNavigateBack,
+    modifier = modifier.fillMaxSize()
+  )
+}
