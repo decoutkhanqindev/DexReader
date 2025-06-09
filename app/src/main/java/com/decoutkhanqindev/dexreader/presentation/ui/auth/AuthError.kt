49ad2bebb0483175f86ea9af6b2e2eeb1e8d8dd2
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-09 16:13:47 +0700

    feat: add AuthError sealed class for handling authentication errors

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/auth/AuthError.kt
@@ -0,0 +1,27 @@
+package com.decoutkhanqindev.dexreader.presentation.ui.auth
+
+sealed class AuthError(val message: String) {
+  sealed class EmailError(message: String) : AuthError(message) {
+    object Invalid : EmailError("Invalid email format.")
+    object Incorrect : EmailError("Incorrect email.")
+    object Required : EmailError("Email is required.")
+    object AlreadyInUse : EmailError("Email already in use.")
+  }
+
+  sealed class PasswordError(message: String) : AuthError(message) {
+    object Weak : PasswordError("Password must be at least 8 characters.")
+    object Incorrect : PasswordError("Incorrect password.")
+    object Required : PasswordError("Password is required.")
+  }
+
+  sealed class ConfirmPasswordError(message: String) : AuthError(message) {
+    object DoesNotMatch : ConfirmPasswordError("Passwords do not match.")
+    object Required : ConfirmPasswordError("Confirm password is required.")
+  }
+
+  sealed class NameError(message: String) : AuthError(message) {
+    object Required : NameError("Name is required.")
+  }
+
+  object UnknownError: AuthError("An unexpected error occurred.")
+}
