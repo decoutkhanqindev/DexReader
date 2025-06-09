7014025a04d197c481e0fb715844ae07719bab80
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-09 16:11:18 +0700

    feat: update RegisterUserUseCase to return User object on registration

--- app/src/main/java/com/decoutkhanqindev/dexreader/domain/usecase/user/RegisterUserUseCase.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/domain/usecase/user/RegisterUserUseCase.kt
@@ -1,11 +1,12 @@
 package com.decoutkhanqindev.dexreader.domain.usecase.user
 
+import com.decoutkhanqindev.dexreader.domain.model.User
 import com.decoutkhanqindev.dexreader.domain.repository.UserRepository
 import javax.inject.Inject
 
 class RegisterUserUseCase @Inject constructor(
   private val userRepository: UserRepository
 ) {
-  suspend operator fun invoke(email: String, password: String): Result<Unit> =
+  suspend operator fun invoke(email: String, password: String): Result<User> =
     userRepository.registerUser(email, password)
 }
