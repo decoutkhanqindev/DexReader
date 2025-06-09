91e94d3681996df4971f902e47f0e89d1f47dadc
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-09 16:16:36 +0700

    feat: refactor LoadingScreen layout to use Box for improved alignment

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/states/LoadingScreen.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/common/states/LoadingScreen.kt
@@ -1,7 +1,9 @@
 package com.decoutkhanqindev.dexreader.presentation.ui.common.states
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,22 +20,27 @@ import com.decoutkhanqindev.dexreader.R
 
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
-  Column(
+  Box(
     modifier = modifier,
-    verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally
+    contentAlignment = Alignment.Center
   ) {
-    Icon(
-      painter = painterResource(R.drawable.app_icon),
-      contentDescription = stringResource(R.string.app_name),
-      modifier = Modifier.size(80.dp)
-    )
-    LinearProgressIndicator(
-      modifier = Modifier
-        .width(100.dp)
-        .padding(top = 8.dp),
-      color = MaterialTheme.colorScheme.onPrimaryContainer,
-      trackColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
-    )
+    Column(
+      modifier = Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Icon(
+        painter = painterResource(R.drawable.app_icon),
+        contentDescription = stringResource(R.string.app_name),
+        modifier = Modifier.size(80.dp)
+      )
+      LinearProgressIndicator(
+        modifier = Modifier
+          .width(100.dp)
+          .padding(top = 8.dp),
+        color = MaterialTheme.colorScheme.onPrimaryContainer,
+        trackColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+      )
+    }
   }
 }
