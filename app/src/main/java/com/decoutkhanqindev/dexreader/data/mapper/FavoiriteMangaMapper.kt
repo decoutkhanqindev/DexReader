477bd4bb3aa73336a3afdb903a5aad3585a8e91c
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-12 00:07:18 +0700

    feat: add FavoriteMangaMapper for converting between FavoriteManga and FavoriteMangaDto

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/data/mapper/FavoiriteMangaMapper.kt
@@ -0,0 +1,24 @@
+package com.decoutkhanqindev.dexreader.data.mapper
+
+import com.decoutkhanqindev.dexreader.data.network.firebase.dto.FavoriteMangaDto
+import com.decoutkhanqindev.dexreader.domain.model.FavoriteManga
+import com.decoutkhanqindev.dexreader.utils.toTimeAgo
+
+fun FavoriteMangaDto.toDomain() =
+  FavoriteManga(
+    id = id,
+    title = title,
+    coverUrl = coverUrl,
+    author = author,
+    status = status,
+    addedAt = addedAt?.time?.toTimeAgo() ?: "Unknown time"
+  )
+
+fun FavoriteManga.toDto() =
+  FavoriteMangaDto(
+    id = id,
+    title = title,
+    coverUrl = coverUrl,
+    author = author,
+    status = status,
+  )
