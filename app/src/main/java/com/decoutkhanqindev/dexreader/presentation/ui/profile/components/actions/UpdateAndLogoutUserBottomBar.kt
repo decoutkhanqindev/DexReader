dadb8dac0d138aa51f2a2c13288cba31a27e4d7c
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-13 14:00:17 +0700

    style: use explicit red color for logout confirmation text

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/profile/components/actions/UpdateAndLogoutUserBottomBar.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/profile/components/actions/UpdateAndLogoutUserBottomBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -67,7 +68,7 @@ fun UpdateAndLogoutUserBottomBar(
             style = MaterialTheme.typography.titleMedium,
             fontWeight = FontWeight.ExtraBold,
             textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.error,
+            color = Color.Red,
             modifier = Modifier.fillMaxWidth()
           )
         },
