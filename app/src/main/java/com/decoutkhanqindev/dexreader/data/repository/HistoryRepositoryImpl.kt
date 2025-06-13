dcab7ebe132d6694f9dfe4c46e520c0d820c970d
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-13 17:31:56 +0700

    feat: implement HistoryRepository and its Firestore-backed implementation

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/data/repository/HistoryRepositoryImpl.kt
@@ -0,0 +1,59 @@
+package com.decoutkhanqindev.dexreader.data.repository
+
+import com.decoutkhanqindev.dexreader.data.mapper.toDomain
+import com.decoutkhanqindev.dexreader.data.mapper.toDto
+import com.decoutkhanqindev.dexreader.data.network.firebase.firestore.FirebaseFirestoreSource
+import com.decoutkhanqindev.dexreader.domain.model.ReadingHistory
+import com.decoutkhanqindev.dexreader.domain.repository.HistoryRepository
+import com.decoutkhanqindev.dexreader.utils.runSuspendCatching
+import com.decoutkhanqindev.dexreader.utils.toResultFlow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class HistoryRepositoryImpl @Inject constructor(
+  private val firebaseFirestoreSource: FirebaseFirestoreSource
+) : HistoryRepository {
+  override fun observeHistory(
+    userId: String,
+    limit: Int,
+    lastReadingHistoryId: String?
+  ): Flow<Result<List<ReadingHistory>>> =
+    firebaseFirestoreSource
+      .observeHistory(
+        userId = userId,
+        limit = limit.toLong(),
+        lastReadingHistoryId = lastReadingHistoryId
+      )
+      .map { readingHistoryDtoList ->
+        readingHistoryDtoList.map { it.toDomain() }
+      }
+      .flowOn(Dispatchers.IO)
+      .distinctUntilChanged()
+      .toResultFlow()
+
+  override suspend fun addAndUpdateToHistory(
+    userId: String,
+    readingHistory: ReadingHistory
+  ): Result<Unit>  =
+    runSuspendCatching(Dispatchers.IO) {
+      firebaseFirestoreSource.addAndUpdateToHistory(
+        userId = userId,
+        readingHistory = readingHistory.toDto()
+      )
+    }
+
+  override suspend fun removeFromHistory(
+    userId: String,
+    readingHistoryId: String
+  ): Result<Unit> =
+    runSuspendCatching(Dispatchers.IO) {
+      firebaseFirestoreSource.removeFromHistory(
+        userId = userId,
+        readingHistoryId = readingHistoryId
+      )
+    }
+}
---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/domain/repository/HistoryRepository.kt
@@ -0,0 +1,14 @@
+package com.decoutkhanqindev.dexreader.domain.repository
+
+import com.decoutkhanqindev.dexreader.domain.model.ReadingHistory
+import kotlinx.coroutines.flow.Flow
+
+interface HistoryRepository {
+  fun observeHistory(
+    userId: String,
+    limit: Int = 20,
+    lastReadingHistoryId: String? = null
+  ): Flow<Result<List<ReadingHistory>>>
+  suspend fun addAndUpdateToHistory(userId: String, readingHistory: ReadingHistory): Result<Unit>
+  suspend fun removeFromHistory(userId: String, readingHistoryId: String): Result<Unit>
+}
