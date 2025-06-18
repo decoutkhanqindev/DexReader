e22b449b865c7394cfa19c14d395c8c1fe5a12dc
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-18 21:33:44 +0700

    refactor: update UI and order of theme options in ThemeSelectorList

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/settings/components/ThemeSelectorList.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/ui/settings/components/ThemeSelectorList.kt
@@ -2,7 +2,8 @@ package com.decoutkhanqindev.dexreader.presentation.ui.settings.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -10,13 +11,16 @@ import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material.icons.filled.PhoneAndroid
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.decoutkhanqindev.dexreader.R
 import com.decoutkhanqindev.dexreader.domain.model.ThemeType
 
@@ -28,9 +32,9 @@ fun ThemeSelectorList(
 ) {
   val themeList = listOf<ThemeItem>(
     ThemeItem(
-      type = ThemeType.SYSTEM,
-      name = stringResource(R.string.system),
-      icon = Icons.Default.PhoneAndroid
+      type = ThemeType.LIGHT,
+      name = stringResource(R.string.light),
+      icon = Icons.Default.LightMode
     ),
     ThemeItem(
       type = ThemeType.DARK,
@@ -38,19 +42,23 @@ fun ThemeSelectorList(
       icon = Icons.Default.DarkMode
     ),
     ThemeItem(
-      type = ThemeType.LIGHT,
-      name = stringResource(R.string.light),
-      icon = Icons.Default.LightMode
+      type = ThemeType.SYSTEM,
+      name = stringResource(R.string.system),
+      icon = Icons.Default.PhoneAndroid
     ),
   )
 
   Card(
     shape = MaterialTheme.shapes.medium,
+    elevation = CardDefaults.cardElevation(8.dp),
     modifier = modifier
   ) {
     LazyColumn(
       verticalArrangement = Arrangement.Center,
-      modifier = modifier
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = Modifier
+        .padding(24.dp)
+        .width(165.dp)
     ) {
       item {
         Text(
@@ -58,7 +66,9 @@ fun ThemeSelectorList(
           style = MaterialTheme.typography.titleLarge,
           fontWeight = FontWeight.ExtraBold,
           textAlign = TextAlign.Center,
-          modifier = Modifier.fillMaxWidth()
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 12.dp)
         )
       }
 
@@ -67,7 +77,7 @@ fun ThemeSelectorList(
           isSelected = selectedThemeType == theme.type,
           theme = theme,
           onSelectedTheme = onSelectedTheme,
-          modifier = Modifier.wrapContentSize()
+          modifier = Modifier.fillMaxWidth()
         )
       }
     }
