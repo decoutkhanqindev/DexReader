ec752f9bf3930764b04eaffea1d7d2faac56b60a
Minh Khang <phamminhkhangpy2003@gmail.com>
2026-02-28 16:36:58 +0700

    feat: Add Firestore collection and field constants for database consistency

---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/data/network/firebase/constant/FirestoreCollections.kt
@@ -0,0 +1,7 @@
+package com.decoutkhanqindev.dexreader.data.network.firebase.constant
+
+object FirestoreCollections {
+  const val USERS = "users"
+  const val FAVORITES = "favorites"
+  const val HISTORY = "history"
+}
---
+++ app/src/main/java/com/decoutkhanqindev/dexreader/data/network/firebase/constant/FirestoreFields.kt
@@ -0,0 +1,6 @@
+package com.decoutkhanqindev.dexreader.data.network.firebase.constant
+
+object FirestoreFields {
+  const val CREATED_AT = "createdAt"
+  const val MANGA_ID = "manga_id"
+}
