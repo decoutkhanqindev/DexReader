6ead8568711796e5c666fe9f15d50ca34413f89e
Minh Khang <phamminhkhangpy2003@gmail.com>
2026-04-19 21:44:31 +0700

    refactor: change NavRoute to sealed interface and update route objects

--- app/src/main/java/com/decoutkhanqindev/dexreader/presentation/navigation/NavRoute.kt
+++ app/src/main/java/com/decoutkhanqindev/dexreader/presentation/navigation/NavRoute.kt
@@ -2,51 +2,51 @@ package com.decoutkhanqindev.dexreader.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
-object NavRoute {
+sealed interface NavRoute {
 
   @Serializable
-  data object Login
+  data object Login : NavRoute
 
   @Serializable
-  data object Register
+  data object Register : NavRoute
 
   @Serializable
-  data object ForgotPassword
+  data object ForgotPassword : NavRoute
 
   @Serializable
-  data object Home
+  data object Home : NavRoute
 
   @Serializable
-  data object Categories
+  data object Categories : NavRoute
 
   @Serializable
-  data object Favorites
+  data object Favorites : NavRoute
 
   @Serializable
-  data object History
+  data object History : NavRoute
 
   @Serializable
-  data object Profile
+  data object Profile : NavRoute
 
   @Serializable
-  data object Settings
+  data object Settings : NavRoute
 
   @Serializable
-  data object Search
+  data object Search : NavRoute
 
   @Serializable
-  data class MangaDetails(val mangaId: String)
+  data class MangaDetails(val mangaId: String) : NavRoute
 
   @Serializable
   data class CategoryDetails(
     val categoryId: String,
     val categoryTitle: String,
-  )
+  ) : NavRoute
 
   @Serializable
   data class Reader(
     val chapterId: String,
     val lastReadPage: Int = 0,
     val mangaId: String,
-  )
+  ) : NavRoute
 }
